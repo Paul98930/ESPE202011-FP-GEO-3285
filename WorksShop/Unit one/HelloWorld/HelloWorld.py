3958ca2a786768a8ba1fc2ae37ad67f6f60bfcc3
PaulIsmaelEscobarCordova <sr.ismael98@gmail.com>
2021-01-08 15:21:11 -0500

    WS01 Hello World in three programming languages

--- WorksShop/Unit one/HelloWorld/HelloWorld.py
+++ WorksShop/Unit one/HelloWorld/HelloWorld.py
@@ -11,4 +11,3 @@ sum = float(addend2) + float(addend2)
 print('The sum of {0} and is {2}'. format(addedn1, addend2, sum))
 
 print('The sum is %.1f' %(float(input('Enter first number: '))+float(input('Enter second number:'))))
-
