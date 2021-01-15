a12b240b6d2a53a91b776e0c3be378c44742f69c
PaulIsmaelEscobarCordova <sr.ismael98@gmail.com>
2021-01-15 14:13:50 -0500

    WS06 Excersice Evaluate

---
+++ WorkShop/Unit_one/WS07/Menu/src/menu/Menu.java
@@ -0,0 +1,60 @@
+
+package menu;
+import java.util.Scanner;
+public class Menu {
+
+    
+    public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+        boolean mainLoop = true;
+        int option;
+
+        do {
+            System.out.println(" ========= Calculator =======");
+            System.out.println("1. -> Addition");
+            System.out.println("2. -> Subtraction");
+            System.out.println("3. -> Multiplication");
+            System.out.println("4. -> Division");
+            System.out.println("5. -> Exit");
+
+            System.out.println("Enter your menu option --> ");
+            option = input.nextInt();
+
+            switch (option) {
+
+                case 1:
+                    int addend1;
+                    int addend2;
+                    int sum;
+                    System.out.println("enter addend 1 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 2 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    break;
+                case 2:
+                    System.out.println("The subtraction is");
+                    break;
+                case 3:
+                    System.out.println("The multiplcation is");
+                    break;
+                case 4:
+                    System.out.println("The division is");
+                    break;
+                case 5:
+                    System.out.println("Good Bye my friend");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option\n\n\n");
+                    break;
+
+            }
+
+        } while (option != 5);
+
+    }
+
+}
