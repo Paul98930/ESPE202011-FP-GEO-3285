e910c3042e74bd9d4707561823c061772261f9bd
Paul Ismael Escobar Cordova <sr.ismael@gmail.com>
2021-01-29 14:27:08 -0500

    WS08

--- WorksShop/Unit one/HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <studio.h>
-int main(){
-    int grade=20;
-    
-    //print() displays information
-    printf("Hello World \t \"  Paul Escobar! \=) \n");
-    printf("\n The grade is %d \n", grade);
-    return 0;
-}
