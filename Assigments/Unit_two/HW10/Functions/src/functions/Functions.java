be73c4b00c613fc21f6bad3a6fc436060c3d4551
Paul Ismael Escobar Cordova <sr.ismael@gmail.com>
2021-02-04 20:36:40 -0500

    HW10

---
+++ Assigments/Unit_two/HW10/Functions/src/functions/Functions.java
@@ -0,0 +1,88 @@
+
+package functions;
+
+import java.util.Scanner;
+
+public class Functions {
+
+    public static void main(String[] args) {
+        
+  System.out.println("******************************************************************************************"); 
+  System.out.println("*Hi, Edison Lascano, PhD. My name's Paul Escobar in this case I show you my homework code*"); 
+  System.out.println("******************************************************************************************");
+  System.out.println("");
+
+  System.out.println("-Give me a number please-. ");
+  System.out.println("------------------------------------------------------------------");
+  System.out.println("");
+        
+  Scanner in = new Scanner(System.in); 
+        int number_one, number_two, number_three, number_four,quotient = 0;
+     
+         
+       System.out.println("The value of the two quotients is = " + quotient); 
+       number_one = in.nextInt();
+       number_two = in.nextInt();
+       division =division(number_one, number_two); 
+    
+       System.out.println("Enter the value, please"); 
+       number_three = in.nextInt();
+
+       System.out.println("Enter the value, please"); 
+       number_four = in.nextInt();
+        int scanner_three = 0;
+
+        Functions g;
+        g = h();
+        System.out.println("The (scanner_three)^2 is" + g);
+        int scanner_four = 0;
+
+        g = h(scanner_four);
+        System.out.println("The multiplication table.");
+
+    }
+
+    private static void division(int number_one, int number_two) {
+        }
+
+    private static Functions h() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+int quotient = 0;
+
+        
+        solution;
+        return quotient;
+
+    private static int h(int scanner_three) { 
+         int g = 0;
+
+        g = scanner_three * scanner_three;
+
+        return g;
+    }
+
+    public static int g(int scanner_four, int g) {
+        var scanner_four = 0;
+        int top = 0;
+        for(int a=1 ; a <= top ; a++){
+            int table = 0;
+            int product = table * a;
+                System.out.println("scanner_four * " + a + " = " + product); 
+        
+    }
+        return g;
+    }
+    private quotient number_one;
+    private quotient number_two;
+
+    
+        }
+    
+
+
+        
+  
+    
+        
+        
