5aff932618ebc13bccbdcd47ba684e3b069643e3
Paul Ismael Escobar Cordova <sr.ismael@gmail.com>
2021-03-31 14:35:22 -0500

    HW21

---
+++ Assigments/Unit_three/HW21/Hucth/src/ec/edu/espe/frmhutch/controller/RabitController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.frmhutch.controller;
+
+import Utils.FileManager;
+import ec.edu.espe.frmhutch.model.Rabit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class RabitController {
+    
+    public String save(Rabit rabit){
+      String data = rabit.getName() + ", " + rabit.getColor() + ", " + rabit.getRace() + ", " + rabit.getSize();
+      FileManager.save(data, "rabits");
+    
+      return "rabits";
+    } 
+    
+    public String read(){
+        
+        String data;
+            data = FileManager.read("rabits");
+      return data; 
+    }
+
+        
+}
---
+++ Assigments/Unit_three/HW21/Hucth/src/ec/edu/espe/frmhutch/model/Rabit.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.frmhutch.model;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Rabit {
+    
+    private String name; 
+    private String race;
+    private String color; 
+    private int size; 
+    private float weight;
+
+    public Rabit(String name, String race, String color, int size, float weight) {
+        this.name = name;
+        this.race = race;
+        this.color = color;
+        this.size = size;
+        this.weight = weight;
+    }
+
+     
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the race
+     */
+    public String getRace() {
+        return race;
+    }
+
+    /**
+     * @param race the race to set
+     */
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the size
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the weight
+     */
+    public float getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+    
+    
+    
+}
